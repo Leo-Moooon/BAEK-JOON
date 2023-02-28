1948821a6fef53925454f8e1d87b4753d81d9059
Leo <leomoon7180@gmail.com>
2023-02-28 20:22:06 +0900

    10869/Node.js/fs모듈 사용/정답

---
+++ 10869(사칙연산)/Node.js/way_fs.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('dev/stdin').toString().split(' ');
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(parseInt(A/B));
+console.log(A%B);
