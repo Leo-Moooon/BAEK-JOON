9928db0d8329e5349b6fd79a501355218ca521fa
Leo <leomoon7180@gmail.com>
2023-02-28 23:16:53 +0900

    2588/Node.js/fs모듈, charAt()  사용/정답2

--- 2588(곱셈)/Node.js/way_fs.js
+++ 2588(곱셈)/Node.js/way_fs.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().split('\n').map(el => parseInt(el));
+const input = fs.readFileSync('dev/stdin').toString().split('\n');
 
-const num1 = input[0];
-const num2 = input[1];
-    const num2_1 = num2%10;
-    const num2_10 = parseInt((num2%100)/10); 
-    const num2_100 = parseInt(num2/100);
+const num1 = parseInt(input[0]);
+const num2 = parseInt(input[1]);
+    const num2_1 = parseInt(input[1].charAt(2));
+    const num2_10 = parseInt(input[1].charAt(1));
+    const num2_100 = parseInt(input[1].charAt(0));
 
-console.log(num1 * num2_1);
-console.log(num1 * num2_10);
-console.log(num1 * num2_100);
-console.log(num1 * num2);
+    console.log(num1 * num2_1);
+    console.log(num1 * num2_10);
+    console.log(num1 * num2_100);
+    console.log(num1 * num2);
