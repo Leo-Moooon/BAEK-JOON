960b92830f18306651094f98da772b49ab4061f9
Leo <leomoon7180@gmail.com>
2023-03-01 19:59:02 +0900

    14681/Node.js/readFileSync(0) 변경/정답

--- 02. 조건문/14681(사분면 고르기)/Node.js/try.js
+++ 02. 조건문/14681(사분면 고르기)/Node.js/try.js
@@ -1,5 +1,11 @@
+/**
+ * readFileSync(0) : 표준 파일 설명자 값(standard input)이 0이기 때문.
+ * readFileSync('dev/stdin') : 별도의 파일로 입력받는 경우
+ *  - 이 문제의 경우 해당 경로에 파일이 없던가, 권한 설정이 안된 경우라고 한다.
+ */
+
 const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().split('\n');
+const input = fs.readFileSync(0).toString().split('\n');
 
 const x = parseInt(input[0]);
 const y = parseInt(input[1]);
