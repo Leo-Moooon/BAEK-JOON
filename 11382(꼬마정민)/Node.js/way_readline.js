14ee0d821c46d5d43a9dfbe7df186f41a0a7c17b
Leo <leomoon7180@gmail.com>
2023-03-01 11:49:40 +0900

    11382/Node.js/readline모듈 사용/정답

---
+++ 11382(꼬마정민)/Node.js/way_readline.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+    input = line.split(' ').map(el => parseInt(el));
+});
+
+rl.on("close", () => {
+    console.log(input[0] + input[1] + input[2]);
+    process.exit();
+});
