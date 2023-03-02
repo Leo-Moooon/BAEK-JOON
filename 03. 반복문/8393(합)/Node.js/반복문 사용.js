2dbfc260586c7649003ebb2f7fe6c3fcc2975685
Leo <leomoon7180@gmail.com>
2023-03-02 17:57:03 +0900

    8393/Node.js/단순화 작업/정답

--- 03. 반복문/8393(합)/Node.js/반복문 사용.js
+++ 03. 반복문/8393(합)/Node.js/반복문 사용.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const input = fs.readFileSync('dev/stdin').toString();
 
 let n = Number(input);
-let arr = [];
 let total = 0;
 for(i = 1; i <= n; i++) {
- arr.push(i);
- total = arr.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+ total += i;
 }
 
 console.log(total);
