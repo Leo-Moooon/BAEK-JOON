1cc27f9cb4a1730acaf3febd3d678ebef7606089
Leo <leomoon7180@gmail.com>
2023-03-02 19:40:18 +0900

    25304/Node.js/답안 참고함/정답/다시 풀어보기

--- 03. 반복문/25304(영수증)/Node.js/try.js
+++ 03. 반복문/25304(영수증)/Node.js/try.js
@@ -1,17 +1,13 @@
 const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().split('\n');
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
 
 const billTotal = Number(input[0]);
 const billQuan = Number(input[1]);
-
-let productPrice = 0;
-let productQuan = 0;
 let sum = 0;
 
-for(let i = 2; i < billQuan; i++) {
-    productPrice = Number((input[i].toString().split(' '))[0]);
-    productQuan = Number((input[i].toString().split(' '))[1]);
-    sum += productPrice * productQuan;
-}
+for(let i = 2; i <= billQuan+1; ++i) {
+    let arr = input[i].split(' ').map(el => Number(el));
+    sum += arr[0] * arr[1];
+};
 
-(billTotal == sum) ? console.log('Yes') : console.log('No');
+console.log(billTotal == sum ? 'Yes' : 'No');
