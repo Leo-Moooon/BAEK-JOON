520800d18a655cd6b0e8e6063dc733645a3e957d
Leo <leomoon7180@gmail.com>
2023-03-02 17:47:12 +0900

    25304/Node.js/for, 삼항연산자 사용/오답2

--- 03. 반복문/25304(영수증)/Node.js/try.js
+++ 03. 반복문/25304(영수증)/Node.js/try.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
 const input = fs.readFileSync('dev/stdin').toString().split('\n');
 
-const totalPrice = Number(input[0]);
-const productNum = Number(input[1]);
+const billTotal = Number(input[0]);
+const billQuan = Number(input[1]);
 
-let priceAndQuantity = 0;
 let productPrice = 0;
-let productQuantity = 0;
-let arr = [];
+let productQuan = 0;
+let sum = 0;
 
-for(i = 2; i< productNum; i++) {
-    priceAndQuantity = input[i].toString().split(' ');
-    productPrice = Number(priceAndQuantity);
-    productQuantity = Number(priceAndQuantity);
-
-    arr.push(productPrice * productQuantity);
+for(let i = 2; i < billQuan; i++) {
+    productPrice = Number((input[i].toString().split(' '))[0]);
+    productQuan = Number((input[i].toString().split(' '))[1]);
+    sum += productPrice * productQuan;
 }
 
-let myTotal = arr.reduce((a, b) => a + b);
-
-(myTotal == totalPrice) ? console.log('Yes') : console.log('No');
+(billTotal == sum) ? console.log('Yes') : console.log('No');
