e7e85f626b5fbd3c7cf9cec6e201bc12824400ef
Leo <leomoon7180@gmail.com>
2023-03-07 23:27:22 +0900

    2480/C#/다중 if문 사용 유의/정답

---
+++ C_sharp/조건문/2480(주사위 세개)/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _2480_주사위_세개_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Array.Sort(str);
+        int A = int.Parse(str[0]);
+        int B = int.Parse(str[1]);
+        int C = int.Parse(str[2]);
+        int result = 0;
+
+        if (A == B && B == C && C == A)
+        {
+            result = 10000 + (A * 1000);
+        }
+        else if (A != B || B != C || C != A)
+        {
+            if (A == B)
+            {
+                result = 1000 + (A * 100);
+            }
+            else if (B == C)
+            {
+                result = 1000 + (B * 100);
+            }
+            else if (C == A)
+            {
+                result = 1000 + (A * 100);
+            }
+            else
+            {
+                result = C * 100;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
+
