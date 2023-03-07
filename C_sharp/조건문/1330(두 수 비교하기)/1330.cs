8bba676b9db25ea3cd4a707dac5d94be09f26f11
Leo <leomoon7180@gmail.com>
2023-03-07 19:30:58 +0900

    1330/C#/if문 규칙은 Node.js와 큰 차이 없음/정답

---
+++ C_sharp/조건문/1330(두 수 비교하기)/1330.cs
@@ -0,0 +1,23 @@
+﻿namespace _1330_두_수_비교하기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        int A = int.Parse(str[0]);
+        int B = int.Parse(str[1]);
+        if (A < B)
+        {
+            Console.WriteLine("<");
+        }
+        else if (A > B)
+        {
+            Console.WriteLine(">");
+        }
+        else
+        {
+            Console.WriteLine("==");
+        }   
+    }
+}
+
