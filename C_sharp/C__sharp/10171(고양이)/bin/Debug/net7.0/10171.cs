548527721c8c743566c7cef2b996d5b23b4991e5
Leo <leomoon7180@gmail.com>
2023-03-07 17:25:26 +0900

    10171/C#/백슬래시 규칙은 Node.js와 동일함/정답

---
+++ C_sharp/C__sharp/10171(고양이)/bin/Debug/net7.0/10171.cs
@@ -0,0 +1,11 @@
+﻿namespace _10171_고양이_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
