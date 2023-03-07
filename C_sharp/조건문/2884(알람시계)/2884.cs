e9ac628438f917bb4da9f0f760d893ec74f501af
Leo <leomoon7180@gmail.com>
2023-03-07 21:37:46 +0900

    2884/C#/Node.js 대비 간결하게 개선함/정답

---
+++ C_sharp/조건문/2884(알람시계)/2884.cs
@@ -0,0 +1,17 @@
+﻿namespace _2884_알람시계_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int h = int.Parse(input[0]);
+        int m = int.Parse(input[1]) - 45;
+
+        if (m < 0)
+        {
+            m += 60;
+            h = h == 0 ? 23 : h-1;
+        }
+            Console.WriteLine(h + " " + m);
+    }
+}
