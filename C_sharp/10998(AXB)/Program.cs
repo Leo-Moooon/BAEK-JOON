9df4fa03e5a47c19352c54aa5b502496788e0bba
Leo <leomoon7180@gmail.com>
2023-03-07 14:07:29 +0900

    10998/C#/정답

---
+++ C_sharp/10998(AXB)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10998_AXB_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
+    }
+}
+
