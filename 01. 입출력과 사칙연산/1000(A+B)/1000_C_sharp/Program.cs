050111caeb88ac7489eae3fd3884359940d24dc9
Leo <leomoon7180@gmail.com>
2023-03-07 13:27:40 +0900

    1000/C#/정답

---
+++ 01. 입출력과 사칙연산/1000(A+B)/1000_C_sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1000_C_sharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+    }
+}
+
