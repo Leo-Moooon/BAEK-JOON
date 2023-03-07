bd2c890d4a725d7c719f2f2b0dc5fa57e19e2b83
Leo <leomoon7180@gmail.com>
2023-03-07 13:50:01 +0900

    1000/C#/정답/파일정리/C#

---
+++ C#/2557(Hello World)/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _2557_Hello_World_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
--- C_sharp/C_sharp/2557(Hello World).cs
+++
@@ -1,9 +0,0 @@
-﻿namespace C_sharp;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
-    }
-}
-
