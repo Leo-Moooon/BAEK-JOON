b42e71c7e92c4258d9db4a41a869c04003f649fa
Leo <leomoon7180@gmail.com>
2023-03-07 17:27:54 +0900

    10172/C#/백슬래시 규칙은 Node.js와 동일/정답

---
+++ C_sharp/C__sharp/10172(개)/10172.cs
@@ -0,0 +1,12 @@
+﻿namespace _10172_개_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
