4f137de964779a13e36dbc1a1f077e98ca270dfd
Leo <leomoon7180@gmail.com>
2023-03-07 15:37:08 +0900

    10869/C#/double 타입에 유의/정답

---
+++ C_sharp/10869(사칙연산)/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _10869_사칙연산_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) - int.Parse(str[1]));
+        Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
+        Console.WriteLine(Math.Floor(double.Parse(str[0]) / double.Parse(str[1])));
+        Console.WriteLine(int.Parse(str[0]) % int.Parse(str[1]));
+    }
+}
