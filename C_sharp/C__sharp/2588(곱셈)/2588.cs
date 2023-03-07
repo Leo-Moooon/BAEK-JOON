599d174c071923a0c14b1cd1aa6cda164365f3c2
Leo <leomoon7180@gmail.com>
2023-03-07 17:11:13 +0900

    2588/C#/GetNumericValue() 메소드 사용/정답/다시 풀어보기

---
+++ C_sharp/C__sharp/2588(곱셈)/2588.cs
@@ -0,0 +1,14 @@
+﻿namespace _2588_곱셈_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+
+        Console.WriteLine(int.Parse(str1) * (int)Char.GetNumericValue(str2[2]));
+        Console.WriteLine(int.Parse(str1) * (int)Char.GetNumericValue(str2[1]));
+        Console.WriteLine(int.Parse(str1) * (int)Char.GetNumericValue(str2[0]));
+        Console.WriteLine(int.Parse(str1) * int.Parse(str2));
+    }
+}
