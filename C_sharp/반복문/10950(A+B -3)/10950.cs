7fc1c0802d5d88ac9265b367ca92747276787150
Leo <leomoon7180@gmail.com>
2023-03-08 08:00:49 +0900

    10950/C#/정답

---
+++ C_sharp/반복문/10950(A+B -3)/10950.cs
@@ -0,0 +1,18 @@
+﻿namespace _10950_A_B__3_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        
+
+        for (int i = 0; i < T; i++)
+        {
+            string[] AB = Console.ReadLine().Split();
+            int A = int.Parse(AB[0]);
+            int B = int.Parse(AB[1]);
+
+            Console.WriteLine(A + B);
+        }
+    }
+}
---
+++ C_sharp/반복문/2739(구구단)/2739.cs
@@ -0,0 +1,13 @@
+﻿namespace _2739_구구단_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.WriteLine(N + " * " + i + " = " + N * i);
+        }
+    }
+}
