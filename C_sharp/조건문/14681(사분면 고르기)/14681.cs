16f9813dc7434746df37e8774742d5543a48f439
Leo <leomoon7180@gmail.com>
2023-03-07 20:42:35 +0900

    14681/C#/삼항연산자 사용/정답

---
+++ C_sharp/조건문/14681(사분면 고르기)/14681.cs
@@ -0,0 +1,23 @@
+﻿namespace _14681_사분면_고르기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int isEven;
+
+        if (y > 0)
+        {
+            isEven = x > 0 ? 1 : 2;
+        }
+        else
+        {
+            isEven = x > 0 ? 4 : 3;
+        }
+
+        Console.WriteLine(isEven);
+    }
+}
+
+
