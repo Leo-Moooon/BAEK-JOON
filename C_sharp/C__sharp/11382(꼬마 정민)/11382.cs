ee75d50efc0ecd6f72e223238775223a0ba7a229
Leo <leomoon7180@gmail.com>
2023-03-07 17:14:29 +0900

    11382/C#/변수타입 유의/정답

---
+++ C_sharp/C__sharp/11382(꼬마 정민)/11382.cs
@@ -0,0 +1,9 @@
+﻿namespace _11382_꼬마_정민_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(double.Parse(str[0]) + double.Parse(str[1]) + double.Parse(str[2]));
+    }
+}
