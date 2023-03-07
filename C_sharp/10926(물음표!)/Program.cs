7ba61e2e3a8497a3f56636a6705db4fd67c45949
Leo <leomoon7180@gmail.com>
2023-03-07 15:40:30 +0900

    10926/C#/정답

---
+++ C_sharp/10926(물음표!)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10926_물음표__;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        Console.WriteLine(str + "??!");
+    }
+}
+
