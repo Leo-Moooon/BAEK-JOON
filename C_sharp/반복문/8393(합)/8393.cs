d9279fbe32e843e0fca9b6db52201b2bce5ce162
Leo <leomoon7180@gmail.com>
2023-03-08 08:06:34 +0900

    8393/C#/정답

---
+++ C_sharp/반복문/8393(합)/8393.cs
@@ -0,0 +1,14 @@
+﻿namespace _8393_합_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            result += i;
+        }
+        Console.WriteLine(result);
+    }
+}
