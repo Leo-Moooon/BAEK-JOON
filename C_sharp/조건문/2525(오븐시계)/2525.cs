a14696aded873e62576366daf790cda259d9ff1e
Leo <leomoon7180@gmail.com>
2023-03-07 23:00:42 +0900

    2525/C#/정답

---
+++ C_sharp/조건문/2525(오븐시계)/2525.cs
@@ -0,0 +1,19 @@
+﻿namespace _2525_오븐시계_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] time = Console.ReadLine().Split();
+        double cook = int.Parse(Console.ReadLine());
+
+        double total = (int.Parse(time[0]) * 60) + int.Parse(time[1]) + cook;
+        double h = Math.Floor(total / 60);
+        double m = total % 60;
+
+        if (h >= 24)
+        {
+            h -= 24;
+        }
+        Console.WriteLine(h + " " + m);
+    }
+}
