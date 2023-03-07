e5890f03630c2b12e403a89e64a81aeea83ba06e
Leo <leomoon7180@gmail.com>
2023-03-07 20:34:18 +0900

    2753/C#/정답

---
+++ C_sharp/조건문/2753(윤년)/2753.cs
@@ -0,0 +1,20 @@
+﻿namespace _2753_윤년_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+        if (year % 400 == 0)
+        {
+            Console.WriteLine(1);
+        }
+        else if (year % 4 == 0 && year % 100 != 0)
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+    }
+}
