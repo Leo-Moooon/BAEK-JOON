01ee10420fe8a714397ce3fae7df5e3d2421808a
Leo <leomoon7180@gmail.com>
2023-03-07 15:54:34 +0900

    10430/C#/정답

---
+++ C_sharp/C__sharp/10430(나머지)/10430.cs
@@ -0,0 +1,15 @@
+﻿namespace _10430_나머지_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        int A = int.Parse(str[0]);
+        int B = int.Parse(str[1]);
+        int C = int.Parse(str[2]);
+        Console.WriteLine((A + B) % C);
+        Console.WriteLine(((A % C) + (B % C)) % C);
+        Console.WriteLine((A * B) % C);
+        Console.WriteLine(((A % C) * (B % C)) % C);
+    }
+}
