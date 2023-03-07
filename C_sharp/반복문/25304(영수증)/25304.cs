9ecae10e4c73416f9bd7136dc7207640ec1ef6c1
Leo <leomoon7180@gmail.com>
2023-03-08 08:16:23 +0900

    25304/C#/정답

---
+++ C_sharp/반복문/25304(영수증)/25304.cs
@@ -0,0 +1,20 @@
+﻿namespace _25304_영수증_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int pay = int.Parse(Console.ReadLine());
+        int N = int.Parse(Console.ReadLine());
+        int total = 0;
+        for (int i = 0; i < N; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int Price = int.Parse(input[0]);
+            int Quan = int.Parse(input[1]);
+
+            total += Price * Quan;
+        }
+        string result = (pay == total) ? "Yes" : "No";
+        Console.WriteLine(result);
+    }
+}
