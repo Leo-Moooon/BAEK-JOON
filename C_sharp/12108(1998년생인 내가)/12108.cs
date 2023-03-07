b6243f927e0a080971503bfee7240aee33da3b46
Leo <leomoon7180@gmail.com>
2023-03-07 15:43:59 +0900

    12108/C#/정답

---
+++ C_sharp/12108(1998년생인 내가)/12108.cs
@@ -0,0 +1,9 @@
+﻿namespace _12108_1998년생인_내가_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+        Console.WriteLine(year - 543);
+    }
+}
