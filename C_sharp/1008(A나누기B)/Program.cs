b494513c9e79156332ab2f2222262686e1d5f9fc
Leo <leomoon7180@gmail.com>
2023-03-07 14:10:45 +0900

    1008/C#/double타입 사용/정답

---
+++ C_sharp/1008(A나누기B)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1008_A나누기B_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] str = Console.ReadLine().Split();
+        Console.WriteLine(double.Parse(str[0]) / double.Parse(str[1]));
+    }
+}
+
