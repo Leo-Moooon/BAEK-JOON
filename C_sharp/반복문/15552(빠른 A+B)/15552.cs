dbc8468ef69139a97544132a1a1d80bc742ef752
Leo <leomoon7180@gmail.com>
2023-03-08 19:13:53 +0900

    15552/C#/정답

---
+++ C_sharp/반복문/15552(빠른 A+B)/15552.cs
@@ -0,0 +1,15 @@
+﻿namespace _15552_빠른_A_B_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double T = double.Parse(Console.ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            string[] str = Console.ReadLine().Split();
+            double A = double.Parse(str[0]);
+            double B = double.Parse(str[1]);
+            Console.WriteLine(A + B);
+        }
+    }
+}
