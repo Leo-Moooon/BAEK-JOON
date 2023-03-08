c038769c80441572346964fffb525bb3493ce49a
Leo <leomoon7180@gmail.com>
2023-03-09 00:53:04 +0900

    2438/C#/정답

---
+++ C_sharp/반복문/2438(별 찍기 -1)/2438.cs
@@ -0,0 +1,17 @@
+﻿namespace _2438_별_찍기__1_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        string star = "";
+        string result = "";
+        for (int i = 0; i < N; i++)
+        {
+            star += "*";
+            result += star + "\n";
+        }
+        Console.WriteLine(result);
+    }
+}
