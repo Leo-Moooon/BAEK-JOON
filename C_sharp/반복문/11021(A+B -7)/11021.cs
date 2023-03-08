00bde08d5c76f60ec5755a0638e3a6e06afe0171
Leo <leomoon7180@gmail.com>
2023-03-09 00:36:42 +0900

    11021/C#/정답2

---
+++ C_sharp/반복문/11021(A+B -7)/11021.cs
@@ -0,0 +1,19 @@
+﻿namespace _11021_A_B__7_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        string result = "";
+
+        for (int i = 1; i <= T; i++)
+        {
+            string[] str = Console.ReadLine().Split();
+            int A = int.Parse(str[0]);
+            int B = int.Parse(str[1]);
+
+            result += "Case #" + i + ": " + (A + B) + "\n";
+        }
+            Console.WriteLine(result);
+    }
+}
