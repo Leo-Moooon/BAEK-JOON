608cb4b0938c1ae175b2ff66c0814a9fc6ac0f63
Leo <leomoon7180@gmail.com>
2023-03-08 12:51:21 +0900

    25314/C#/정답

---
+++ C_sharp/반복문/25314(코딩은 체육과목입니다)/25314.cs
@@ -0,0 +1,15 @@
+﻿namespace _25314_코딩은_체육과목입니다_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string result = "";
+
+        for (int i = 1; i <= N / 4; i++)
+        {
+            result += "long ";
+        }
+        Console.WriteLine(result + "int");
+    }
+}
