4978883ed6f16b49ef15535d6a98cf4f01e879f4
Leo <leomoon7180@gmail.com>
2023-03-09 01:00:56 +0900

    2439/C#/중첩 for/정답

---
+++ C_sharp/반복문/2439(별 찍기 -2)/2439.cs
@@ -0,0 +1,25 @@
+﻿namespace _2439_별_찍기__2_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        string space = "";
+        string star = "";
+        string result = "";
+
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = N; j > i; j--)
+            {
+                space += " ";
+            }
+            star += "*";
+            result += space + star + "\n";
+            space = "";
+        }
+        Console.WriteLine(result);
+    }
+}
+
