8f5b79cb9cce884a118143330a8aab4befbb621f
Leo <leomoon7180@gmail.com>
2023-03-09 00:45:31 +0900

    11022/C#/아이디어는 11021과 동일/정답

---
+++ C_sharp/반복문/11022/11022.cs
@@ -0,0 +1,20 @@
+﻿namespace _11022;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        string result = "";
+
+        for (int i = 1; i <= T; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            result += "Case #" + i + ": " + A + " + " + B + " = " + (A + B) + "\n";
+        }
+        Console.WriteLine(result);
+    }
+}
+
