6144f9ba1728530694b53d4cd85005d864b16d5f
Leo <leomoon7180@gmail.com>
2023-03-09 23:11:38 +0900

    10807/C#/foreach 적극활용/정답

--- C_sharp/1차원 배열/10807(개수 세기)/10807.cs
+++ C_sharp/1차원 배열/10807(개수 세기)/10807.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Linq;
-namespace _10807_개수_세기_;
+﻿namespace _10807_개수_세기_;
 class Program
 {
     static void Main(string[] args)
     {
         int N = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split();
+        int v = int.Parse(Console.ReadLine());
+        int count = 0;
 
+        foreach (string el in input)
+        {
+            int num = int.Parse(el);
+            if( v == num) count++;
+        }
+        Console.WriteLine(count);
     }
 }
-
