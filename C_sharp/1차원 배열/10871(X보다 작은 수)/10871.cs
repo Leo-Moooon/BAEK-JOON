1963dfd929f856c684275da593c6004360ca6b7b
Leo <leomoon7180@gmail.com>
2023-03-09 23:31:15 +0900

    10871/C#/foreach문 활용/ 정답

---
+++ C_sharp/1차원 배열/10871(X보다 작은 수)/10871.cs
@@ -0,0 +1,22 @@
+﻿namespace _10871_X보다_작은_수_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] NX = Console.ReadLine().Split();
+        int N = int.Parse(NX[0]);
+        int X = int.Parse(NX[1]);
+        string[] A = Console.ReadLine().Split();
+        string result = default;
+
+        foreach (string i in A)
+        {
+            int num = int.Parse(i);
+            if (num < X)
+            {
+                result += num + " ";
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
