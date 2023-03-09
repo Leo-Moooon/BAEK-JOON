314c86fe1fdb55ee0eec7dece933eb84d5406260
Leo <leomoon7180@gmail.com>
2023-03-09 22:28:23 +0900

    파일명 변경

---
+++ C_sharp/1차원 배열/10807(개수 세기)/10807.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+namespace _10807_개수_세기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+    }
+}
+
