8fff18b63968317ca786040f4b760248fa1ca2c2
Leo <leomoon7180@gmail.com>
2023-03-10 00:26:29 +0900

    2562/C#/for문/정답

---
+++ C_sharp/1차원 배열/2562(최댓값)/2562.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2562_최댓값_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int max = default;
+        int count = default;
+
+        for (int i = 0; i < 9; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num > max)
+            {
+                max = num;
+                count = i + 1;
+            }
+        }
+        Console.WriteLine(max + "\n" + count);
+    }
+}
