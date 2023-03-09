34c9b0ee7f26eb68a6f46bcd852b961033eebede
Leo <leomoon7180@gmail.com>
2023-03-09 23:45:19 +0900

    10818/C#/Array.ConvertAll() 사용/정답

---
+++ C_sharp/1차원 배열/10818(최소, 최대)/10818.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace _10818_최소__최대_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split();
+        int[] ints = Array.ConvertAll(input, i => int.Parse(i));
+        string result = ints.Min().ToString() + " " + ints.Max().ToString();
+
+        Console.WriteLine(result);
+    }
+}
