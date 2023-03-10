a96d051660fa9047c3c0d94bd6d2377445c68b45
Leo <leomoon7180@gmail.com>
2023-03-10 14:42:11 +0900

    10813/C#/정답

---
+++ C_sharp/1차원 배열/10813(공 바꾸기)/10813.cs
@@ -0,0 +1,35 @@
+﻿namespace _10813_공_바꾸기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] NM = Console.ReadLine().Split();
+        int N = int.Parse(NM[0]);
+        int M = int.Parse(NM[1]);
+        int[] ints = new int[N];
+
+        for (int a = 1; a <= N; a++)
+        {
+            ints[a-1] = a;
+        }
+
+        for (int a = 0; a < M; a++)
+        {
+            string[] input = Console.ReadLine().Split();
+            int i = int.Parse(input[0]);
+            int j = int.Parse(input[1]);
+
+            int tmp = ints[i-1];
+            ints[i-1] = ints[j-1];
+            ints[j-1] = tmp;
+        }
+
+        string result = default;
+
+        foreach (int a in ints)
+        {
+            result += a + " ";
+        }
+        Console.WriteLine(result);
+    }
+}
