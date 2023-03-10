5a2c893e1bfe29c941cfedd150963ee9e71d08e3
Leo <leomoon7180@gmail.com>
2023-03-10 11:11:23 +0900

    10810/C#/단계 구분해서 공략/정답

---
+++ C_sharp/1차원 배열/10810(공 넣기)/10810.cs
@@ -0,0 +1,31 @@
+﻿namespace _10810_공_넣기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] NM = Console.ReadLine().Split();
+        int N = int.Parse(NM[0]);
+        int M = int.Parse(NM[1]);
+        int[] ints = new int[N];
+        string result = default;
+        for (int a = 0; a < M; a++)
+        {
+            string[] ijk = Console.ReadLine().Split();
+            int i = int.Parse(ijk[0]);
+            int j = int.Parse(ijk[1]);
+            int k = int.Parse(ijk[2]);
+
+            for (int b = i-1; b < j; b++)
+            {
+                ints[b] = k;
+            }
+        }
+        string[] iToStr = Array.ConvertAll(ints, el => el.ToString());
+
+        foreach (string c in iToStr)
+        {
+            result += c + " ";
+        }
+        Console.WriteLine(result);
+    }
+}
