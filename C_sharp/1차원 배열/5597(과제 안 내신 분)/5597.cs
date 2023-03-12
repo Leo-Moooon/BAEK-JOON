512e2d349398f75992cad81bd67ad6c3095487a8
Leo <leomoon7180@gmail.com>
2023-03-13 00:40:54 +0900

    3397/C#/잘 생각해냈다/정답

---
+++ C_sharp/1차원 배열/5597(과제 안 내신 분)/5597.cs
@@ -0,0 +1,29 @@
+﻿namespace _5597_과제_안_내신_분_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] student = new int[30];
+        int input;
+        string result = default;
+
+        for (int i = 0; i < 30; i++)
+        {
+            student[i] = (i+1);
+        }
+
+        for (int j = 0; j < 28; j++)
+        {
+            input = int.Parse(Console.ReadLine());
+            student = student.Where(el => el != input).ToArray();
+        }
+
+        foreach (int k in student)
+        {
+            result += k + "\n";
+        }
+
+        Console.WriteLine(result);
+    }
+}
+
