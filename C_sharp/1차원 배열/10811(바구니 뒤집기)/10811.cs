e5d5d340bcfa5a43175cd6aaab397111db2c0d9f
Leo <leomoon7180@gmail.com>
2023-03-13 01:24:59 +0900

    10811/C#/Reverse메소드 사용, 파라미터 설정에 유의/정답

---
+++ C_sharp/1차원 배열/10811(바구니 뒤집기)/10811.cs
@@ -0,0 +1,38 @@
+﻿namespace _10811_바구니_뒤집기_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 입력 단계
+        string[] NM = Console.ReadLine().Split();
+        int N = int.Parse(NM[0]);
+        int M = int.Parse(NM[1]);
+        string result = default;
+        int[] basket = new int[N];
+
+        for (int a = 0; a < N; a++)
+        {
+            basket[a] = a + 1;
+        }
+        // 뒤집기 단계
+        for (int b = 0; b < M; b++)
+        {
+            string[] ij = Console.ReadLine().Split();
+
+            if (ij == null) break;
+
+            int i = int.Parse(ij[0])-1;
+            int j = int.Parse(ij[1])-1;
+
+            Array.Reverse(basket, i, j-i+1);
+        }
+
+        // 출력 단계
+        foreach (int c in basket)
+        {
+            result += c + " ";
+        }
+
+        Console.WriteLine(result);
+    }
+}
--- C_sharp/1차원 배열/3052(나머지)/3052.cs
+++ C_sharp/1차원 배열/3052(나머지)/3052.cs
@@ -23,4 +23,3 @@ class Program
         Console.WriteLine(newNum.Length);
     }
 }
-
