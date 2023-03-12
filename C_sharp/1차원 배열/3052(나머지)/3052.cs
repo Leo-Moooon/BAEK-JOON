01944b87f98ed96fb1814df8cd626533d6734214
Leo <leomoon7180@gmail.com>
2023-03-13 01:04:48 +0900

    3052/C#/ Distinct()를 사용하면 매우 쉬운 문제/정답

---
+++ C_sharp/1차원 배열/3052(나머지)/3052.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+namespace _3052_나머지_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 변수 선언 단계
+        int[] num = new int[10];
+        int[] newNum = default;
+
+
+        // 42로 나눈 나머지 구하는 단계
+        for (int i = 0; i < 10; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+            num[i] = input % 42;
+        }
+
+        // 중복 제거 단계
+        newNum = num.Distinct().ToArray();
+
+        // 출력 단계
+        Console.WriteLine(newNum.Length);
+    }
+}
+
