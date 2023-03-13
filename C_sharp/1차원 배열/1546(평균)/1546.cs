1006acde754cf640c9e0c2906b5fb53b97910f15
Leo <leomoon7180@gmail.com>
2023-03-13 13:57:13 +0900

    1346/C#/Sort() 사용/정답/1차원 배열 끝

---
+++ C_sharp/1차원 배열/1546(평균)/1546.cs
@@ -0,0 +1,33 @@
+﻿namespace _1546_평균_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 입력 단계 
+        int N = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split();
+        double result = default;
+
+        // 점수 오름차순으로 정렬 및 최고점 확인 
+        double[] scores = Array.ConvertAll(input, el => double.Parse(el));
+        Array.Sort(scores);
+        double max = scores[^1];
+
+
+        // 점수 조작
+        for (int i = 0; i < N; i++)
+        {
+            scores[i] = (scores[i] / max) * 100;
+        }
+
+        // 평균 도출
+        foreach (double j in scores)
+        {
+            result += j;
+        }
+
+        // 출력
+        Console.WriteLine(result/N);
+    }
+}
+
